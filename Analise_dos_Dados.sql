9d172d867fd309d5a5ea7397e2bbfca3c2534b08
Davi Martins <davibarroso12399@gmail.com>
2023-10-26 15:58:51 -0300

    Ordenando os meses

--- Analise_dos_Dados.sql
+++ Analise_dos_Dados.sql
@@ -6,12 +6,18 @@ GROUP BY hotel;
 
 #Qual mês (no segundo semestre de 2018) cada hotel teve mais reservas efetivadas (O guest apareceu no hotel e saiu do hotel)?
 SELECT  hotel as "Hotel", 
-		arrival_date_month as "Mês", 
+        arrival_date_month as "Mês", 
         COUNT(reservation_status) as "Total de Reservas Efetivadas"
         FROM hotel_revenue
 WHERE reservation_status = "Check-Out"
 GROUP BY hotel, arrival_date_month, reservation_status
-ORDER BY arrival_date_month, COUNT(reservation_status);
+ORDER BY CASE WHEN arrival_date_month = "July" then 1
+WHEN arrival_date_month = "August" then 2
+WHEN arrival_date_month = "September" then 3
+WHEN arrival_date_month = "October" then 4
+WHEN arrival_date_month = "November" then 5
+WHEN arrival_date_month = "December" then 6
+ELSE NULL END;
 
 #Nacionalidade que mais visita cada Hotel.
 SELECT hotel, country,  COUNT(country) as "Visitantes no Segundo Semestre de 2018" FROM hotel_revenue
